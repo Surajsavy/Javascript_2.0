98e03c6c1ff7deffabeafbe1001a5600a783d52c
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-09-20 18:31:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all JS code as new version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3
+
+    + 3) // code readablity should be high 
+
+console.log("Suraj")
+
+
+let name = "suraj"
+let age = 23
+let isLoggedIn = "false"
+let state;
+
+//  number  =>  2 to power 53 
+// bigInt
+// string => ""
+// boolean => true/false
+// null   => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined 
+
+
