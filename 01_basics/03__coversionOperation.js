d3b999d6d8560d8b322b814820dafe76c1e50222
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-09-21 05:28:37 +0000

    coversion of Datatypes

---
+++ 01_basics/03__coversionOperation.js
@@ -0,0 +1,26 @@
+let score = true
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueinNumber = Number(score)
+//console.log(typeof (valueinNumber))
+//console.log(valueinNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+ let isLoggedIn = "Suraj"
+ let booleanisLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleanisLoggedIn)
+
+ // 1 => true; 0 => false
+ // "" => false ; 
+ //"Suraj" => true
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
