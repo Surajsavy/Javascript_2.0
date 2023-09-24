70e6581851fb4c5ac99e60e85fb3d451c5e72d5b
Surajsavy <93027224+Surajsavy@users.noreply.github.com>
2023-09-24 08:09:40 +0000

    more_codePushed

--- 01_basics/03__coversionOperation.js
+++ 01_basics/03__coversionOperation.js
@@ -43,3 +43,14 @@ let str3 = str1 + str2
 //console.log(1+"2")
 //console.log("1" + 2 + 2)
 //console.log(1+2+"2")
+//console.log(+true)
+//console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+//console.log(num2)
+
+let gameCounter =100
+//gameCounter++
+++gameCounter;
+console.log(gameCounter)
